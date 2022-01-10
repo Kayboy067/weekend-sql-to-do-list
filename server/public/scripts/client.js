8f26d59ad766377b3b9dd6c970f901d43c9d9e93
Kayode Adebanjo <kay.adebanjo@gmail.com>
2022-01-09 22:05:02 -0600

    get and post to render datas to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,71 @@
 
-$(document).ready(onReady);
+$(document).ready(onReady)
 
 function onReady() {
-    console.log("JQ");
+    console.log(('JQ'));
+    $('#addButton').on('click', addDailyTask);
+    getTask();
+}// end function inReady
+
+
+
+
+
+function addDailyTask(evt) {
+    evt.preventDefault()
+    let taskInput = {
+        task: $('#dailyTask').val(),
+        date: $('#dateIs').val(),
+        importance: $('#importanceId').val()
+    }
+    console.log('here is taskInput', taskInput);
+
+    // setup the endpoint (AJAX)
+
+    $.ajax({
+        type: 'POST',
+        url: '/todos',
+        data: taskInput
+    }).then( function (response) {
+        console.log(response);
+        $('#dailyTask').val(''),
+        $('#dateIs').val(''),
+        $('#importanceId').val(''),
+        getTask();
+    })
+    .catch(function(err) {
+        console.log('error in POST', err)
+    });
     
-}
+} // end function addDailyTask
+
+function render(array) {
+    $('#tableBody').empty();
+    // loop through an array to append data to DOM
+    for (let todo of array) {
+    $('#tableBody').append(`
+        <tr data-id="${todo.id}">
+            <td>${todo.task}</td>
+            <td>${todo.importance}</td>
+            <td>${todo.date}</td>
+            <td><button class="statusButton">Completed</button><td>
+            <td><button class="deleteButton">Delete</button><td>
+        </tr>
+    `)
+    }
+} // end function renderTask
+
+function getTask(){
+    console.log('in getTask');
+    //ajax GET call to server to get task
+    $.ajax({
+        type: 'GET',
+        url: '/todos'
+    }).then(function (response){
+        console.log('response from server is', response);
+        render(response)
+    })
+    .catch(function (error){
+        console.log('error in GET', error)
+    })
+} // end function getTask
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const todo = express.Router();
-console.log(todo);
+const todoRouter = express.Router();
+
 const pg = require('pg');
 
 // DB CONNECTION
@@ -10,4 +10,55 @@ const pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
 });
-console.log(pool);
+
+// Get all todos
+
+todoRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM todos;';
+    pool
+        .query(queryText)
+        .then(result => {
+        // sends back the results in an object
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error getting todo', error);
+            res.sendStatus(500)
+        });
+});
+
+// POST for todos 
+// Add a new task
+todoRouter.post('/', (req, res) => {
+    console.log('this is re.body', req.body);
+    let queryText =`
+    INSERT INTO "todos" 
+	("task", "date", "importance") 
+VALUES
+    ($1, $2, $3)
+    `;
+    let queryParams = [
+        req.body.task,
+        req.body.date,
+        req.body.importance
+    ];
+    console.log('queryText is', queryText);
+    pool.query(queryText, queryParams)
+    .then(result => {
+        res.sendStatus(201);
+    })
+    .catch(error => {
+        console.log('error getting todo', error);
+        res.sendStatus(500)
+    })
+});
+
+
+// PUT ... update a task to show it has been completed
+
+
+
+
+// DELETE ... Remove a task when delete button is pressed after completion
+
+module.exports = todoRouter;
